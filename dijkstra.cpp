e123249243007de4667a8e0efe6e6e4d31a2e5b7
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2018-12-10 15:01:16 +0700

    Add files via upload

---
+++ dijkstra.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef  pair<int,int> ii ;
+typedef  vector<int> vi;
+typedef  vector<pair<int,int>> vii;
+#define INF 1000000000
+#define pb emplace_back
+#define mkp make_pair
+int main()
+{
+    int V,E,s,u,v,w;
+    cin >> V >> E >> s;
+    vector<vii> adj;
+    adj.assign(V+5,vii());
+    for(int i=0;i<E;i++){
+        cin >> u >> v >> w;
+        adj[u].pb(ii(v,w)); // directed graph
+    }
+    vi dist(V+5,INF);
+    dist[s]=0;
+    priority_queue<ii,vector<ii>,greater<ii>> pq;
+    pq.emplace(ii(0,s));
+    while(pq.size()){
+        ii fr=pq.top();
+        pq.pop();
+        int d=fr.first;
+        int u=fr.second;
+        if(d>dist[u]) continue;
+        for(auto j:adj[u]){
+            int src=j.first;
+            int ww=j.second;
+            if(dist[u]+ww<dist[src]){
+                dist[src]=dist[u]+ww;
+                pq.emplace(ii(dist[src],src));
+            }
+        }
+    }
+    for (int i = 0; i < V; i++) // index + 1 for final answer
+        printf("SSSP(%d, %d) = %d\n", s, i, dist[i]);
+}
