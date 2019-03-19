bd5953d26deed4e316d3fe3a3bad7ab78c42a8d6
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2019-03-19 18:52:49 +0700

    Add files via upload

---
+++ detectingcycle_undirectedgraph_indegreeoutdegree.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+using namespace std;
+vector<int> adj[600000];
+int deg[600000];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N,M;
+	cin >> N >> M;
+	for(int i=1;i<=M;i++)
+	{
+		int u,v;
+		cin >> u >> v ;
+		adj[u].emplace_back(v);
+		adj[v].emplace_back(u);
+		deg[u]++;
+		deg[v]++;
+	}
+	queue<int> q;
+	for(int i=1;i<=N;i++)
+	{
+		if(deg[i]==1) q.emplace(i);
+	}
+	while(!q.empty())
+	{
+		int u = q.front();
+		q.pop();
+		for(auto v : adj[u])
+		{
+			if(deg[v]>0)
+			{
+				deg[u]--;
+				if(--deg[v]==1)
+				{
+					q.emplace(v);
+				}
+			}
+		}
+	}
+	for(int i=1;i<=N;i++)
+	{
+		cout << deg[i] << "\n";
+	}
+	//cout << "NO LOOP";
+}
+//doesn't test with strong testcase
